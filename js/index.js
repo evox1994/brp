1d8c8b2a4b478a7361b13dc450920817fab46b01
evox1994 <dmitrybatukhtin@gmail.com>
2018-11-10 18:05:29 +0700

    sdfvfdvfv

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+	$('.b-1-slider').slick();
+	$('.b-7-slider').slick({
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1280,
+				settings: {
+					slidesToShow: 3
+				}
+			},
+			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+
+	$('.b-7-slide a').click(function(){
+		var src = $(this).attr('href');
+		$('.b-7-image img').attr('src',src);
+		return false;
+	});
+
+});
---
+++ js/main.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+
+	function scrollHeader(){
+		if ( $(window).width() > 767 ) {
+			var st = $(window).scrollTop();
+			if ( st > 0 ) {
+				$('.header').addClass('active');
+			} else {
+				$('.header').removeClass('active');
+			}
+		} else {
+			$('.header').removeClass('active');
+		}
+	}
+	scrollHeader();
+
+	$(window).scroll(function(){
+		scrollHeader();
+	});
+
+	$('input[type="tel"]').inputmask('+7 (999) 999-99-99');
+
+	$('.radio-btn').click(function(){
+		if ( $(this).hasClass('active') ) {
+			$(this).removeClass('active');
+		} else {
+			$(this).removeClass('error');
+			$(this).addClass('active');
+		}
+	});
+
+});
