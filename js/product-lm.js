fd6a5888da3c3f95a4c55edaea20f9b95897103d
evox1994 <dmitrybatukhtin@gmail.com>
2018-11-21 18:05:36 +0700

    sdfvsdfv

---
+++ js/product-lm.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+	$('.b-4-slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1280,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+	
+	$('.b-6-slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1280,
+				settings: {
+					slidesToShow: 2
+				}
+			},
+			{
+				breakpoint: 500,
+				settings: {
+					slidesToShow: 1
+				}
+			}
+		]
+	});
+
+	$('.par').click(function(){
+		if ( $(this).hasClass('active') ) {
+			$(this).removeClass('active');
+			$(this).parents('li').find('.par-wrap').removeClass('active');
+		} else {
+			$(this).addClass('active');
+			$(this).parents('li').find('.par-wrap').addClass('active');
+		}
+	});
+
+});
